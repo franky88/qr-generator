a58803b61bcd08b3c703bd9c00f4e60367c4bb37
frank <ramosfp88@gmail.com>
2021-06-03 10:04:05 +0800

    fixing fonts

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;800&display=swap');
 #app {
-  font-family: "Montserrat" Avenir, Helvetica, Arial, sans-serif !important; 
+  font-family: "Montserrat" sans-serif; 
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
